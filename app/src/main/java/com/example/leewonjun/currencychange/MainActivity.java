793aebb16d4687c692950b378f68fbc8cc56bce3
mingming <21jun7654@gmail.com>
2018-01-26 07:53:11 +0900

    change Toast message

--- app/src/main/java/com/example/leewonjun/currencychange/MainActivity.java
+++ app/src/main/java/com/example/leewonjun/currencychange/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "클릭!", Toast.LENGTH_SHORT).show();
             }
         });
     }
